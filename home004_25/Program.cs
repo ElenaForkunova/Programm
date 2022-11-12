a6ad531dd14413a898c4b65a9d160c8bcfef6794
Elena Forkunova <forkelena@yandex.ru>
2022-11-13 00:27:47 +0400

    добавила натуральности B

--- home004_25/Program.cs
+++ home004_25/Program.cs
@@ -5,20 +5,20 @@
 3, 5 -> 243 (3⁵)
 2, 4 -> 16 */
 
-int GetFac(int number1, int number2)
+int GetFact(int number1, int number2)
 {
-    int fac = 1;
+    int fact = 1;
     number2 = Math.Abs(number2);
     for (int i = 1; i <= number2; ++i)
     {
-        fac = number1 * fac;
+        fact = number1 * fact;
     }
-    return fac;
+    return fact;
 }
 
 Console.WriteLine("Введите числа А и В: ");
 int number1 = Convert.ToInt32(Console.ReadLine());
 int number2 = Convert.ToInt32(Console.ReadLine());
 
-int fac = GetFac(number1, number2);
-Console.WriteLine(fac);
+int fact = GetFact(number1, number2);
+Console.WriteLine(fact);
